e3ef68d53dc68760c94656de04e1109ca0b8cdc2
xin.zhou <zhouxin576@gmail.com>
2021-09-09 23:37:21 +0800

    complete get request

--- server.py
+++ server.py
@@ -1,97 +1,200 @@
-from os import write
 import typing
 import asyncio
 import logging
+import h11
+from urllib.parse import unquote
 
 logging.basicConfig(
-                level=logging.DEBUG,
-                format='%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s',
-                datefmt='%a, %d %b %Y %H:%M:%S',
-                filemode='w')
+    level=logging.DEBUG,
+    format="%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s",
+    datefmt="%a, %d %b %Y %H:%M:%S",
+    filemode="w",
+)
 
 logger = logging.getLogger()
 
-# server
-# async def serve(callback: typing.Callable, host: str="127.0.0.1", port: int=5000):
 
-#     await asyncio.start_server(callback, host, port)
-#     while True:
-#         await asyncio.sleep(0.1)
-
-# # application
-# async def handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-
-#     data = await reader.read(1000)
-#     msg = data.decode()
-
-#     logger.info(f"Received data:")
-#     logger.info(f"begin -----------------------------")
-#     logger.info(f"\n{msg}")
-#     logger.info(f"end -----------------------------")
-
-#     send_data = "pong"
-#     writer.write(send_data.encode())
-#     await writer.drain()
-#     logger.info(f"Send: {send_data}")
-#     logger.info(f"Connection close")
-#     writer.close()
+def is_ssl(transport: asyncio.Transport) -> bool:
+    return bool(transport.get_extra_info("sslcontext"))
 
 
-class Request:
+class H11Protocol(asyncio.BaseProtocol):
+    def __init__(self, app: typing.Callable, on_response: typing.Callable) -> None:
+        self.app = app
+        self.conn = h11.Connection(h11.SERVER)
+        self.loop = asyncio.get_event_loop()
+        self.on_response = on_response
 
-    def __init__(self, method: str, router: str, headers: dict = {}):
+        # per-conn
+        self.transport = None
+        self.server = None
+        self.client = None
 
-        self.method = method
-        self.router = router
+        # per-request
+        self.scope = None
+        self.headers = None
+        self.cycle = None
+
+    def data_received(self, buffer: bytes):
+        self.conn.receive_data(buffer)
+        self.startup()
+
+    def connection_made(self, transport):
+
+        self.server = transport.get_extra_info("socket").getsockname()[0]
+        self.client = transport.get_extra_info("socket").getpeername()[0]
+        self.transport = transport
+
+    def eof_received(self):
+        pass
+
+    def startup(self):
+
+        while True:
+            try:
+                event = self.conn.next_event()
+            except h11.RemoteProtocolError:
+                return
+
+            _type = type(event)
+            if _type is h11.NEED_DATA:
+                break
+
+            elif _type is h11.PAUSED:
+                break
+
+            elif _type is h11.Request:
+                raw_path, _, query_string = event.target.partition(b"?")
+                self.scope = {
+                    "type": "http",
+                    "asgi": {
+                        "version": "3.0",
+                        "spec_version": "2.1",
+                    },
+                    "http_version": event.http_version.decode("ascii"),
+                    "server": self.server,
+                    "client": self.client,
+                    "method": event.method.decode("ascii"),
+                    "path": unquote(raw_path.decode("ascii")),
+                    "raw_path": raw_path,
+                    "query_string": query_string,
+                    "headers": [(key.lower(), value) for key, value in event.headers],
+                }
+
+                self.cycle = Cycle(
+                    scope=self.scope,
+                    conn=self.conn,
+                    transport=self.transport,
+                    headers=event.headers,
+                    message_event=asyncio.Event(),
+                    on_response=self.on_response,
+                )
+
+                self.loop.create_task(self.cycle(app))
+
+            elif _type is h11.EndOfMessage:
+                self.cycle.message_event.set()
+
+
+class Cycle:
+    def __init__(
+        self,
+        scope,
+        conn,
+        transport,
+        headers,
+        message_event,
+        on_response,
+    ) -> None:
+        self.scope = scope
+        self.conn = conn
+        self.transport = transport
         self.headers = headers
+        self.message_event = message_event
+        self.on_response = on_response
+        self.response_complete = False
 
-    def __str__(self) -> str:
-        
-        return f"method = {self.method}, router = {self.router}, headers = {self.headers}"
+    async def __call__(self, app) -> typing.Any:
+        await app(self.scope, self.receive, self.send)
+
+    async def send(self, message):
+        message_type = message["type"]
+        if message_type == "http.response.body":
+            body = message.get("body", b"")
+            event = h11.Data(data=body)
+            output = self.conn.send(event)
+            self.transport.write(output)
+            self.response_complete = True
+            event = h11.EndOfMessage()
+            output = self.conn.send(event)
+            self.transport.write(output)
+
+        elif message_type == "http.response.start":
+            status_code = message["status"]
+            headers = self.headers
+            reason = b"OK"
+            event = h11.Response(
+                status_code=status_code, headers=headers, reason=reason
+            )
+            output = self.conn.send(event)
+            self.transport.write(output)
+
+        if self.response_complete:
+            event = h11.ConnectionClosed()
+            self.conn.send(event)
+            self.transport.close()
+            self.on_response()
+
+    async def receive(self):
+
+        # if not self.response_complete:
+        #     await self.message_event.wait()
+        #     self.message_event.clear()
+
+        # if self.response_complete:
+        #     message = {"type": "http.disconnect"}
+
+        # else:
+        #     message = {
+        #         "type": "http.request",
+        #         "body": self.body,
+        #         "more_body": self.more_body,
+        #     }
+        #     self.body = b""
+
+        # return message
+        pass
+
 
 # server
 class Server:
-
-    def __init__(self, app: typing.Callable) -> None:
+    def __init__(
+        self, app: typing.Callable, host: str = "127.0.0.1", port: int = 5000
+    ) -> None:
         self.app = app
-        self.host = "127.0.0.1"
-        self.port = 5000
+        self.host = host
+        self.port = port
 
     async def handler(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
 
-        data = await reader.read(100)
-        msg = data.decode()
+        # read all data
 
+        loop = asyncio.get_event_loop()
+        connection_lost = loop.create_future()
 
-        logger.info(f"Received data:")
-        logger.info(f"begin -----------------------------")
-        logger.info(f"\n{msg}")
-        logger.info(f"end -----------------------------")
+        transport = writer.transport
+        protocol = H11Protocol(
+            self.app, on_response=lambda: connection_lost.set_result(True)
+        )
+        transport.set_protocol(protocol)
 
-        req = self.parse_http_request(msg)
-        logger.info(f"parsed request = {req}")
-        res = await self.app(req)
-        writer.write(res.encode())
-        await writer.drain()
-        logger.info(f"Send: {res}")
-        logger.info(f"Connection close")
-        writer.close()
+        protocol.connection_made(transport)
+        data = reader._buffer  # type: ignore
 
-    def parse_http_request(self, msg: str):
+        if data:
+            protocol.data_received(data)
 
-        lines = msg.split("\n")
-        head = lines[0]
-        method, uri, _ = head.split(" ")
-        headers = {}
-        for line in lines[1:]:
-            if not line:
-                continue
-            if ": " not in line:
-                continue
-            key, value = line.split(": ")
-            headers[key] = value
-
-        return Request(method, uri, headers)
+        await connection_lost
 
     async def serve(self):
 
@@ -100,31 +203,117 @@ class Server:
             await asyncio.sleep(0.1)
 
 
-# application
-async def app(req: Request) -> str:
+class Request:
+    def __init__(self, scope: dict) -> None:
+        self.scope = scope
+
+    @property
+    def method(self):
+        return self.scope["method"]
+
+    @property
+    def scheme(self):
+        return self.scope["scheme"]
+
+    @property
+    def query_string(self):
+        query = self.scope["query_string"].decode()
+        return {
+            key: value
+            for key, value in [
+                raw_single_query.split("=") for raw_single_query in query.split("#")
+            ]
+        }
+
+    @property
+    def headers(self):
+        return {key.decode(): value.decode() for key, value in self.scope["headers"]}
 
 
-    if req.router == "/":
-        return "root router"
+class Response:
+    def __init__(self, content: str, status_code: int) -> None:
+        self.body = content.encode()
+        self.status_code = status_code
 
-    elif req.router == "/abc":
-        return "handling abc"
-    elif req.router == "/ping":
-        msg = (
-            "HTTP/1.1 200 OK\r\n"
-            "date: Wed, 18 Aug 2021 15:17:41 GMT\r\n"
-            "server: uvicorn\r\n"
-            "content-length: 14\r\n"
-            "\r\n"
-            "hello, world"
-            "\r\n"
+    def __str__(self) -> str:
+        return f"Response(content={self.body!r},status_code={self.status_code})"
+
+
+class Route:
+    def __init__(self, path: str, endpoint: typing.Callable) -> None:
+        self.path = path
+        self.endpoint = endpoint
+
+    async def __call__(
+        self,
+        scope: dict,
+        receive: typing.Callable,
+        send: typing.Callable,
+    ) -> None:
+
+        request = Request(scope)
+        res: Response = await self.endpoint(request)
+        logger.info(f"res = {res}")
+        await send(
+            {
+                "type": "http.response.start",
+                "status": res.status_code,
+            }
         )
-        return msg
-    else:
-        return "handleing others"
+
+        await send({"type": "http.response.body", "body": res.body})
+
+    def match(self, url: str) -> bool:
+        return url == self.path
+
+
+class Router:
+    class NotFound(Exception):
+        pass
+
+    def __init__(self, routes: typing.List[Route]) -> None:
+        self.routes = routes
+
+    async def __call__(
+        self,
+        scope: dict,
+        receive: typing.Callable,
+        send: typing.Callable,
+    ) -> None:
+
+        url = scope["path"]
+        for r in self.routes:
+            if r.match(url):
+                return await r(scope, receive, send)
+
+        raise self.NotFound()
+
+
+class Application:
+    def __init__(self, router: Router) -> None:
+        self.router = router
+
+    async def __call__(
+        self,
+        scope: dict,
+        receive: typing.Callable,
+        send: typing.Callable,
+    ) -> None:
+
+        return await self.router(scope, receive, send)
+
+
+# business
+async def hello(request: Request) -> Response:
+
+    name = request.query_string.get("name")
+    return Response(f"hello, {name}", 200)
+
+
+router = Router([Route("/", hello)])
+app = Application(router)
 
 
 if __name__ == "__main__":
     server = Server(app)
     asyncio.run(server.serve())
-
